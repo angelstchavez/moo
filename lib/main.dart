67c68902dc0040b2d823e354d797ecd9c875602e
angelstchavez <angelstchavez@gmail.com>
2024-04-15 10:27:29 -0500

    feat: add initial firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: "AIzaSyBOkRQvsxoUvvG_k-3kxY8p55fjwegINTM",
+      appId: "1:173997870344:web:fda390459bc809ff7530bd",
+      messagingSenderId: "173997870344",
+      projectId: "moo-app-6485e",
+    ));
+  }
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
